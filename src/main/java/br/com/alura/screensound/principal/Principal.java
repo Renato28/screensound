e94dd3399c23952da59300ad10c0a5f7f98d9dae
Renato28 <renatonobrega87@gmail.com>
2024-04-22 17:39:01 -0300

    implementando metodo exibir menu e criando metodo para cadastrar um artista

--- src/main/java/br/com/alura/screensound/principal/Principal.java
+++ src/main/java/br/com/alura/screensound/principal/Principal.java
@@ -2,14 +2,46 @@ package br.com.alura.screensound.principal;
 
 import br.com.alura.screensound.repository.ArtistaRepository;
 
+import java.util.Scanner;
+
 public class Principal {
 
     private final ArtistaRepository repositorio;
+    private Scanner leitura = new Scanner(System.in);
 
     public Principal(ArtistaRepository repositorio) {
         this.repositorio = repositorio;
     }
 
     public void exibeMenu() {
+
+        var opcao = -1;
+
+        while (opcao!= 9) {
+            var menu = """
+                    *** Screen Sound Músicas ***                    
+                                        
+                    1- Cadastrar artistas                
+                    9 - Sair
+                    """;
+
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+            leitura.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    cadastrarArtistas();
+                    break;
+                case 9:
+                    System.out.println("Encerrando a aplicação!");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        }
+    }
+
+    private void cadastrarArtistas() {
     }
 }
